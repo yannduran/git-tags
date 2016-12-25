3bcdcf46ceb7b49320229309eb86d5cf13f9ae80
Yann Duran <yd@live.com.au>
2016-12-26 02:56:30 +1100

    Add Package class, add image resources, set auto-sync on (using Extensibility Tools)
    - Package.class.cs (nested under Package.Vsct)
    
    - Package.ico
    - preview.png
    
    - Package.vsct (Set auto-sync VSCT commands)
    - source.extension.vsixmanifest (Auto-sync resx and icon files)

---
+++ src/pkg/Package.class.cs
@@ -0,0 +1,16 @@
+﻿namespace GitTags.Package
+{
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    using static PackageGuids;
+    using static Vsix;
+
+
+    [InstalledProductRegistration("110", "112", Version, IconResourceID = 400)]
+    [Guid(PackageString)]
+
+    public sealed class Package
+    {
+    }
+}
